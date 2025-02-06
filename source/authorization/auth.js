32e363741f6a365989cfa41dcb7dc2a2eef7598c
Yanter44 <zhmih44@gmail.com>
2025-02-06 15:17:13 +0300

    явахуе8

--- source/authorization/auth.js
+++ source/authorization/auth.js
@@ -6,34 +6,27 @@ document.addEventListener("DOMContentLoaded", async function () {
         // Формируем URL с параметрами
         const url = `https://6c14-178-173-127-190.ngrok-free.app/api/Data/GetToken`;
 
-        const response = await fetch(url, {
-            method: "GET",
-        });
+    const response = await fetch(url, {
+    method: "GET",
+});
 
-        // Проверка на успешный статус ответа
-        if (!response.ok) {
-            console.error("Ошибка запроса: ", response.status, response.statusText);
-            return;
-        }
+if (!response.ok) {
+    throw new Error(`Ошибка на сервере: ${response.status} ${response.statusText}`);
+}
 
-        // Логирование ответа от сервера
-        const text = await response.text();  // Получаем ответ как текст
-        console.log("Response Text:", text);  // Логируем текст ответа
+const text = await response.text();  // Получаем ответ как текст
+console.log("Response Text:", text);
 
-        // Пробуем преобразовать в JSON
-        try {
-            const data = JSON.parse(text);  
-            if (data.token) {
-                localStorage.setItem("token", data.token);
-                window.location.href = "/firstpage";
-            } else {
-                console.error("Ошибка авторизации:", data.error);
-            }
-        } catch (jsonError) {
-            console.error("Ошибка при парсинге JSON:", jsonError);
-        }
-
-    } catch (error) {
-        console.error("Ошибка при выполнении запроса:", error);
+try {
+    const data = JSON.parse(text);  // Пробуем преобразовать в JSON
+    if (data.token) {
+        localStorage.setItem("token", data.token);
+        window.location.href = "/firstpage";
+    } else {
+        console.error("Ошибка авторизации:", data.error);
     }
+} catch (jsonError) {
+    console.error("Ошибка при парсинге JSON:", jsonError);
+}
+
 });
