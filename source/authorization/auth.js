60897186ecdb7672fe6ec6d633436ff09d439d0c
Yanter44 <zhmih44@gmail.com>
2025-02-06 16:10:13 +0300

    явахуе20

--- source/authorization/auth.js
+++ source/authorization/auth.js
@@ -1,47 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Функция для извлечения параметров из URL
-    function getUrlParams() {
-        const urlParams = new URLSearchParams(window.location.search);
-        const initData = urlParams.get('initData');
-        const initDataSignature = urlParams.get('initDataSignature');
-        const username = urlParams.get('username');  // предполагается, что это тоже передается в URL
-        const telegramId = urlParams.get('telegramId');  // и это также
-
-        console.log({ initData, initDataSignature, username, telegramId }); // Проверяем параметры
-        return { initData, initDataSignature, username, telegramId };
-    }
-
-    // Функция авторизации через Telegram
-    async function telegramAuth(initData, initDataSignature, username, telegramId) {
-        const response = await fetch('https://cbf4-178-173-127-190.ngrok-free.app/api/Data/TelegramAuth', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                initData: initData,
-                initDataSignature: initDataSignature,
-                username: username,
-                telegramId: telegramId
-            })
-        });
-
-        const responseData = await response.json();
-        if (response.ok) {
-            console.log('Authorization successful', responseData);
-            // Здесь можно перенаправить пользователя или обновить интерфейс
-        } else {
-            console.error('Authorization failed', responseData);
+window.onload = function () {
+    fetch('https://cbf4-178-173-127-190.ngrok-free.app/api/data/GetToken', {
+        method: 'GET', // HTTP method
+        headers: {
+            'Content-Type': 'application/json',
         }
-    }
-
-
-    const { initData, initDataSignature, username, telegramId } = getUrlParams();
-
-    // Если все параметры есть, выполняем авторизацию
-    if (initData && initDataSignature && username && telegramId) {
-        telegramAuth(initData, initDataSignature, username, telegramId);
-    } else {
-        console.error('Missing parameters in the URL.');
-    }
-});
+    })
+    .then(response => response.json()) // Parse the response as JSON
+    .then(data => {
+        if (data.token) {
+            console.log('Token received:', data.token);
+            // Redirect to index.html after success
+            window.location.href = '/index';
+        } else {
+            console.error('No token received');
+        }
+    })
+    .catch(error => {
+        console.error('Error fetching the token:', error);
+    });
+};
