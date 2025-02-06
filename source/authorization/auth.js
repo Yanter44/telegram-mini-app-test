a0b6ca3a44c8cc5bc6994021e98c23bf82b3cfb3
Yanter44 <zhmih44@gmail.com>
2025-02-06 22:37:58 +0300

    явахуе45

--- source/authorization/auth.js
+++ source/authorization/auth.js
@@ -1,43 +1,51 @@
 window.onload = function () {
-    const initData = Telegram.WebApp.initData;
-    const initDataSignature = Telegram.WebApp.initDataSignature;
-    
-   
-    const telegramId = Telegram.WebApp.initDataUnsafe.user.id; 
-    const username = Telegram.WebApp.initDataUnsafe.user.username; 
+    if (Telegram && Telegram.WebApp && Telegram.WebApp.initDataUnsafe) {
+        const initData = Telegram.WebApp.initData;
+        const initDataSignature = Telegram.WebApp.initDataSignature;
 
-    console.log('InitData:', initData);
-    console.log('InitDataSignature:', initDataSignature);
-    console.log('TelegramId:', telegramId);
-    console.log('Username:', username);
+        if (Telegram.WebApp.initDataUnsafe.user) {
+            const telegramId = Telegram.WebApp.initDataUnsafe.user.id; // ID пользователя
+            const username = Telegram.WebApp.initDataUnsafe.user.username; // Имя пользователя
 
-    // Формируем объект для отправки на сервер
-    const requestData = {
-        initData: initData,
-        initDataSignature: initDataSignature,
-        username: username,
-        telegramId: telegramId
-    };
+            console.log('InitData:', initData);
+            console.log('InitDataSignature:', initDataSignature);
+            console.log('TelegramId:', telegramId);
+            console.log('Username:', username);
 
-    fetch('https://1f2b-178-173-127-190.ngrok-free.app/api/Data/TelegramAuth', {  
-        method: 'POST',
-        headers: {
-            'ngrok-skip-browser-warning': 'true',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(requestData)
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.token) {
-            console.log('Получен токен:', data.token);
-            localStorage.setItem('authToken', data.token);
-            window.location.href = '/firstpage'; // или другая страница
+      
+            const requestData = {
+                initData: initData,
+                initDataSignature: initDataSignature,
+                username: username,
+                telegramId: telegramId
+            };
+
+            fetch('https://1f2b-178-173-127-190.ngrok-free.app/api/Data/TelegramAuth', {
+                method: 'POST',
+                headers: {
+                    'ngrok-skip-browser-warning': 'true',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(requestData)
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.token) {
+                    console.log('Получен токен:', data.token);
+                  
+                    localStorage.setItem('authToken', data.token);
+                    window.location.href = '/firstpage'; 
+                } else {
+                    console.error('Ошибка авторизации:', data.error);
+                }
+            })
+            .catch(error => {
+                console.error('Ошибка при запросе авторизации:', error);
+            });
         } else {
-            console.error('Ошибка авторизации:', data.error);
+            console.error('Нет данных о пользователе');
         }
-    })
-    .catch(error => {
-        console.error('Ошибка при запросе авторизации:', error);
-    });
+    } else {
+        console.error('Telegram WebApp не инициализирован');
+    }
 };
