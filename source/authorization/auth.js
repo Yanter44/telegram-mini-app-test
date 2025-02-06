f7d46f7da073518409d0a07a9126a24222711ab3
Yanter44 <zhmih44@gmail.com>
2025-02-06 14:15:21 +0300

    Обновление структуры проекта

--- source/authorization/auth.js
+++ source/authorization/auth.js
@@ -1,35 +1,40 @@
-document.addEventListener("DOMContentLoaded", async function() {
-    const initData = window.Telegram.WebApp.initData; // Получаем данные Telegram
-    const initDataSignature = window.Telegram.WebApp.initDataSignature; // Получаем подпись данных
+document.addEventListener("DOMContentLoaded", async function () {
+    const platform = window.Telegram.WebApp.platform; // Определяем платформу
+
+    if (platform !== "android" && platform !== "ios") {
+        console.error("Доступ разрешен только с мобильного приложения Telegram!");
+        alert("Вход разрешен только с мобильного Telegram.");
+        return;
+    }
+    const API_BASE_URL = "https://b3d1-178-173-127-190.ngrok-free.app";
+
+    const initData = window.Telegram.WebApp.initData;
+    const initDataSignature = window.Telegram.WebApp.initDataSignature;
 
     if (!initData || !initDataSignature) {
         console.error("Ошибка: initData или initDataSignature не найдены!");
         return;
     }
 
- 
-
-    // Отправляем данные на сервер для проверки
-    const response = await fetch('http://localhost:5103/api/Data/telegram-auth', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ 
-            initData: initData,
-            initDataSignature: initDataSignature,
-            username: userData.username,  // Дополнительные данные о пользователе
-            telegramId: userData.id
-        })
-    });
+    const response = await fetch(`${API_BASE_URL}/api/Data/telegram-auth`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+        initData: initData,
+        initDataSignature: initDataSignature,
+        username: userData.username,
+        telegramId: userData.id,
+    }),
+});
 
     const data = await response.json();
 
     if (data.token) {
-        localStorage.setItem('token', data.token);
-        window.location.href = '/dashboard'; // Перенаправляем на нужную страницу после успешной авторизации
+        localStorage.setItem("token", data.token);
+        window.location.href = "/dashboard";
     } else {
         console.error("Ошибка авторизации:", data.error);
     }
 });
-
