23d6d5151a8fc5f87c1878e5060a638579bbb751
Yanter44 <zhmih44@gmail.com>
2025-02-15 20:16:02 +0300

    ngrokhttpscheck

--- source/authorization/auth.js
+++ source/authorization/auth.js
@@ -19,7 +19,7 @@ window.onload = function () {
                 telegramId: telegramId.toString()
             };
 
-            fetch('https://eb06-178-173-127-152.ngrok-free.app/api/Data/TelegramAuth', {
+            fetch('https://4bb3-178-173-127-152.ngrok-free.app/api/Data/TelegramAuth', {
                 method: 'POST',
                 headers: {
                     'ngrok-skip-browser-warning': 'true',
