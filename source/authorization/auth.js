368299661797918261b3e9e4882f67a9436e065a
Yanter44 <zhmih44@gmail.com>
2025-02-17 19:58:36 +0300

    xxxxxxf

--- source/authorization/auth.js
+++ source/authorization/auth.js
@@ -1,31 +1,24 @@
 window.onload = function () {
-    // Проверка наличия Telegram WebApp
     if (Telegram && Telegram.WebApp && Telegram.WebApp.initDataUnsafe) {
-        // Получаем необработанные данные initData через WebApp
         const initData = Telegram.WebApp.initData;
-        // Получаем необработанные данные из SDK, если доступно
-        const { initDataRaw } = retrieveLaunchParams();  // Это извлекает initDataRaw из SDK
+        const initDataSignature = Telegram.WebApp.initDataSignature;
 
-        // Логируем данные
-        console.log('InitData:', initData);
-        console.log('InitDataRaw:', initDataRaw);  // Логируем необработанные данные
-
-        // Проверка наличия данных о пользователе
         if (Telegram.WebApp.initDataUnsafe.user) {
-            const telegramId = Telegram.WebApp.initDataUnsafe.user.id;  // ID пользователя
-            const username = Telegram.WebApp.initDataUnsafe.user.username;  // Имя пользователя
+            const telegramId = Telegram.WebApp.initDataUnsafe.user.id; // ID пользователя
+            const username = Telegram.WebApp.initDataUnsafe.user.username; // Имя пользователя
 
+            console.log('InitData:', initData);
+            console.log('InitDataSignature:', initDataSignature);
             console.log('TelegramId:', telegramId);
             console.log('Username:', username);
 
-            // Подготовка данных для запроса
+      
             const requestData = {
-                initData: initDataRaw,  // Отправляем initData
+                initData: initData,           
                 username: username,
                 telegramId: telegramId.toString()
             };
 
-            // Отправляем POST запрос на сервер
             fetch('https://eb55-45-32-144-56.ngrok-free.app/api/Data/TelegramAuth', {
                 method: 'POST',
                 headers: {
@@ -39,10 +32,9 @@ window.onload = function () {
             .then(data => {
                 if (data.token) {
                     console.log('Получен токен:', data.token);
-                    // Сохраняем токен в localStorage
+                  
                     localStorage.setItem('authToken', data.token);
-                    // Перенаправляем на другую страницу
-                    window.location.href = '/firstpage.html';
+                    window.location.href = '/firstpage.html'; 
                 } else {
                     console.error('Ошибка авторизации:', data.error);
                 }
