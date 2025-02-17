2b0be359e5d23bbf91ed7c34a83ba8f78be113a5
Yanter44 <zhmih44@gmail.com>
2025-02-17 18:23:07 +0300

    kakoejedrochevo

--- source/authorization/auth.js
+++ source/authorization/auth.js
@@ -1,21 +1,21 @@
 window.onload = function () {
     if (Telegram && Telegram.WebApp && Telegram.WebApp.initDataUnsafe) {
-        const initDataUnsafe = Telegram.WebApp.initDataUnsafe;
-        const initData = initDataUnsafe.initData;
-        const initDataSignature = initDataUnsafe.initDataSignature;
+        const initData = Telegram.WebApp.initData;
+        const initDataSignature = Telegram.WebApp.initDataSignature;
 
-        if (initDataUnsafe.user) {
-            const telegramId = initDataUnsafe.user.id;
-            const username = initDataUnsafe.user.username;
+        if (Telegram.WebApp.initDataUnsafe.user) {
+            const telegramId = Telegram.WebApp.initDataUnsafe.user.id; // ID пользователя
+            const username = Telegram.WebApp.initDataUnsafe.user.username; // Имя пользователя
 
             console.log('InitData:', initData);
             console.log('InitDataSignature:', initDataSignature);
             console.log('TelegramId:', telegramId);
             console.log('Username:', username);
 
+      
             const requestData = {
                 initData: initData,           
-                initDataSignature: initDataSignature, 
+                signature: initDataSignature,
                 username: username,
                 telegramId: telegramId.toString()
             };
@@ -33,6 +33,7 @@ window.onload = function () {
             .then(data => {
                 if (data.token) {
                     console.log('Получен токен:', data.token);
+                  
                     localStorage.setItem('authToken', data.token);
                     window.location.href = '/firstpage.html'; 
                 } else {
